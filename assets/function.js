f1a6e6c9249dee7e77ffcc92ec79bba0f18ce481
Joseph Greenwell <joeygreenwell@ymail.com>
2025-05-15 19:07:27 -0500

    adding in actual photos and restructuring html

--- assets/function.js
+++ assets/function.js
@@ -2,7 +2,7 @@ let currentSlide = 0;
 const slides = document.querySelectorAll('.slide');
 const dots = document.querySelectorAll('.dot');
 const totalSlides = slides.length;
-let interval = setInterval(nextSlide, 4000);
+let interval = setInterval(nextSlide, 6000);
 
 function showSlide(index) {
   slides.forEach((slide, i) => {
